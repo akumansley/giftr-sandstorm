ff4829bba975f735d6dff521df6d33bb3854cca0
Mitchell Barry <mitch.barry@gmail.com>
2015-12-15 06:37:27 -0800

    Allow optional URLs when adding gift to list

--- client/lists-show.js
+++ client/lists-show.js
@@ -1,3 +1,11 @@
+function isValidUrl(url) {
+    // Copyright (c) 2010-2013 Diego Perini, MIT licensed
+    // https://gist.github.com/dperini/729294
+    // see also https://mathiasbynens.be/demo/url-regex
+    // modified to allow protocol-relative URLs
+    return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test( url );
+}
+
 Template.listsShow.helpers({
     items: function() {
         return Items.find({listId: this._id}, {sort: {createdAt: 1}});
@@ -31,10 +39,26 @@ Template.listsShow.events({
     "submit #add-item" : function(e, t) {
         e.preventDefault();
 
-        var text = t.$('[name=item-text]').val();
+        var item = {
+            text: t.$('[name=item-text]').val(),
+            url: t.$('[name=item-url]').val()
+        };
 
-        Meteor.call('addItem', text, this._id);
+        // Perform simple validations:
+        // 1. Require a name for gift
+        // 2. If URL provided, ensure it is a real address
+        if (!item.text) {
+            alert("Please enter a gift name");
+            return false;
+        }
+        if (item.url && !isValidUrl(item.url)) {
+            alert("Please provide a valid URL");
+            return false;
+        }
+
+        Meteor.call('addItem', item, this._id);
         t.$('[name=item-text]').val('');
+        t.$('[name=item-url]').val('');
     },
     "submit .add-comment" : function(e) {
         e.preventDefault();
